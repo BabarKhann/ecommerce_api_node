de9a7cb781f13817637dabad7b26f90d34bb3d74
BabarKhann <babarkhan138@gmail.com>
2023-09-12 00:54:06 +0500

    Initialize Project, express setup and create /products route to load data from products.json file

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const fs = require('fs').promises;
+const path = require('path');
+const app = require('express')();
+
+const port = process.env.PORT || 1337;
+
+app.get('/products', listProducts);
+
+app.listen(port, () => {
+    console.log(`server listening on ${port}`)
+});
+
+async function listProducts(req, res) {
+    const productsFile = path.join(__dirname, './products.json')
+
+    try {
+        const data = await fs.readFile(productsFile);
+
+        res.json(JSON.parse(data));
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
