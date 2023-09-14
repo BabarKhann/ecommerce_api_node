74b3060ed9e021ab56cd7c82aeaffd4e97f5d8bd
BabarKhann <babarkhan138@gmail.com>
2023-09-15 01:21:22 +0500

    Add bootstrap cdn, render products with fetch api, and style with boostrap grid and card

--- app.js
+++ app.js
@@ -15,9 +15,12 @@ async function listProducts(req, res) {
     const productsFile = path.join(__dirname, './products.json')
 
     try {
+
         const data = await fs.readFile(productsFile);
 
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.json(JSON.parse(data));
+
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
