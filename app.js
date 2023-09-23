9283ffcd902e3be8afd3dc2ef43313ea82f31759
BabarKhann <babarkhan138@gmail.com>
2023-09-24 02:59:41 +0500

    update query parameters (limit & offset) in api & product model

--- app.js
+++ app.js
@@ -3,25 +3,13 @@ const fs = require('fs').promises;
 const path = require('path');
 const app = require('express')();
 
+const api = require('./api');
+
 const port = process.env.PORT || 1337;
 
-app.get('/products', listProducts);
+// routes
+app.get('/products', api.listProducts);
 
 app.listen(port, () => {
     console.log(`server listening on ${port}`)
 });
-
-async function listProducts(req, res) {
-    const productsFile = path.join(__dirname, './products.json')
-
-    try {
-
-        const data = await fs.readFile(productsFile);
-
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.json(JSON.parse(data));
-
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
-}
